c63e1342ff69b868343289bef4a590a694af5b23
venom26 <54362015+venom26@users.noreply.github.com>
2022-04-13 18:17:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -224,23 +224,25 @@ function install_go_tools() {
 		echo -e "$GREEN""[+] Installing Go tools from Github.""$NC";
 		sleep 1;
 		echo -e "$GREEN""[+] Installing subfinder from Github.""$NC";
-		go get -u github.com/subfinder/subfinder;
+		go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest;
 		echo -e "$GREEN""[+] Installing subjack from Github.""$NC";
-		go get -u github.com/haccer/subjack;
+		go install -v github.com/haccer/subjack@latest;
 		echo -e "$GREEN""[+] Installing ffuf from Github.""$NC";
-		go get -u github.com/ffuf/ffuf;
+		go install -v github.com/ffuf/ffuf@latest;
 		echo -e "$GREEN""[+] Installing gobuster from Github.""$NC";
-		go get -u github.com/OJ/gobuster;
+		go install github.com/OJ/gobuster/v3@latest;
 		# echo -e "$GREEN""[+] Installing inception from Github.""$NC";
 		# go get -u github.com/proabiral/inception;
 		echo -e "$GREEN""[+] Installing waybackurls from Github.""$NC";
-		go get -u github.com/tomnomnom/waybackurls;
+		go install -v github.com/tomnomnom/waybackurls@latest;
 		echo -e "$GREEN""[+] Installing goaltdns from Github.""$NC";
-		go get -u github.com/subfinder/goaltdns;
+		go install -v github.com/subfinder/goaltdns@latest;
 		echo -e "$GREEN""[+] Installing rescope from Github.""$NC";
-        go get -u github.com/root4loot/rescope;
+                go install -v github.com/root4loot/rescope@latest;
 		echo -e "$GREEN""[+] Installing httprobe from Github.""$NC";
-		go get -u github.com/tomnomnom/httprobe;
+		go install -v github.com/tomnomnom/httprobe@latest;
+                echo -e "$GREEN""[+] Installing Amass from Github.""$NC";
+		go install -v github.com/OWASP/Amass/v3/...@master;
 }
 
 function install_go() {
@@ -248,13 +250,14 @@ function install_go() {
 				echo -e "$GREEN""[i] Go is already installed, skipping installation.""$NC";
 				return;
 		fi
-		echo -e "$GREEN""[+] Installing Go 1.12 from golang.org.""$NC";
-		wget -nv https://dl.google.com/go/go1.12.linux-amd64.tar.gz;
+		echo -e "$GREEN""[+] Installing latest Go from golang.org.""$NC";
+		version=$(curl -L -s https://golang.org/VERSION?m=text);
+		wget https://dl.google.com/go/${version}.linux-amd64.tar.gz
 		sudo tar -C /usr/local -xzf go1.12.linux-amd64.tar.gz;
 		echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin:" >> "$HOME"/.profile;
 		echo "export GOPATH=$HOME/go" >> "$HOME"/.profile;
 		source "$HOME"/.profile;
-		rm -rf go1.12.linux-amd64.tar.gz;
+		rm -rf go1*;
 }
 
 function install_amass() {
